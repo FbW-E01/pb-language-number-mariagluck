3499317a4f62ca36234baae2830381c478fdb054
mariagluck <mariamart.berlin@gmail.com>
2021-04-01 09:53:57 +0200

    language numbers task

---
+++ numbers.js
@@ -0,0 +1,22 @@
+// Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
+// ```javascript
+// let introSentence = "hello, my name is Fran and I am";
+// let age = 25;
+// ```
+
+let introPhrase = "Hi, I am an AI and my number is ";
+let numberCode = 371;
+console.log(introPhrase + numberCode);
+
+// `+` operator is concatenating a string ana number
+
+// #### 2. Converting 
+// * Create a variable with the value of "1005". Convert it to a number.
+ const number = "1005";
+console.log(parseInt("1005"));
+
+// * Create a variable with the value of "10.05". Convert it to a number. 
+const specificNumber = "10.05";
+console.log(parseFloat("10.05"));
+
+
