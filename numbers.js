a5e59aca6762bef2963a3572eef82830394b696e
mariagluck <mariamart.berlin@gmail.com>
2021-04-01 10:01:18 +0200

    comment typo edited

--- numbers.js
+++ numbers.js
@@ -1,14 +1,10 @@
 // Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
-// ```javascript
-// let introSentence = "hello, my name is Fran and I am";
-// let age = 25;
-// ```
 
 let introPhrase = "Hi, I am an AI and my number is ";
 let numberCode = 371;
 console.log(introPhrase + numberCode);
 
-// `+` operator is concatenating a string ana number
+// `+` operator is concatenating a string and a number
 
 // #### 2. Converting 
 // * Create a variable with the value of "1005". Convert it to a number.
